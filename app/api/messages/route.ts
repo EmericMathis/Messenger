4c8405ecb241ca86bab09ac0e7a42b42f922c71a
emeric mathis <emericmathis@gmail.com>
2024-05-17 18:24:25 +0200

    sync creation of conversation

--- app/api/messages/route.ts
+++ app/api/messages/route.ts
@@ -2,6 +2,8 @@ import getCurrentUser from "@/app/actions/getCurrentUser";
 import { NextResponse } from "next/server";
 import prisma from "@/app/libs/prismadb"
 
+import { pusherServer } from "@/app/libs/pusher";
+
 export async function POST(
     request: Request
 ) {
@@ -67,6 +69,18 @@ export async function POST(
                 }
             }
         })
+
+        await pusherServer.trigger(conversationId, 'messages:new',  newMessage)
+
+       const lastMessage = updatedConversation.messages[updatedConversation.messages.length - 1]
+
+       updatedConversation.users.map((user) => {
+        pusherServer.trigger(user.email!, 'converation:update', {
+            id:conversationId,
+            messages: [lastMessage]
+        })
+       })
+
         return NextResponse.json(newMessage)
     } catch (error:any) {
         console.log(error,'ERROR_MESSAGES');
